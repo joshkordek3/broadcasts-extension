0ba97d354e6049b674d91c5151d4c8cfe61b6ad1
Joshua Kordek <79750252+joshkordek3@users.noreply.github.com>
2021-04-07 10:24:10 +0800

    I rage quit

--- main.ts
+++ main.ts
@@ -1,36 +1,23 @@
 //% color=#37E0FF weight=0 icon="\uf1d8" block="Broadcasts"
 namespace Broadcasts {
-    let broadcat_active = -1
-    //% shim=ENUM_GET2938
-    //% blockId=broadcasts82932938283929323232323
-    //% block="%Broadcat"
-    //% enumName="Broadcats"
-    //% enumMemberName="Broadcat"
-    //% enumPromptHint="Broadcast..."
-    //% enumInitialMembers="message1"
-    //% blockHidden=true
-    export function dummy (Broadcat: number) {
-        return Broadcat
-    }
+    let broadcat_active: any = NaN
     //% blockId=stuff032382932
     //% block="broadcast %broadcat"
-    //% broadcat.shadow="broadcasts82932938283929323232323"
-    export function broadcast (broadcat: number) {
+    export function broadcast (broadcat: string) {
         broadcat_active = broadcat
     }
     //% blockId=stuff032382932898988
     //% block="I receive %broadcast"
-    //% broadcat.shadow="broadcasts82932938283929323232323"
-    export function broadcast_receive2 (broadcat: number) {
+    export function broadcast_receive2 (broadcat: string) {
         return broadcat_active == broadcat
     }
     //% blockId=stuff032382932898
     //% block="when I receive %broadcast"
     //% broadcat.shadow="broadcasts82932938283929323232323"
-    export function broadcast_receive (broadcat: number, a: () => void) {
+    export function broadcast_receive (broadcat: string, a: () => void) {
         basic.forever(function () {
             if (broadcast_receive2(broadcat)) {
-                broadcat_active = -1
+                broadcat_active = NaN
                 control.inBackground(a)
             }
         })
