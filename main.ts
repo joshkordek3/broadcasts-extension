5c1fba249bf24cff425e405cf07c83e51a3ee2fa
Joshua Kordek <79750252+joshkordek3@users.noreply.github.com>
2021-04-07 09:51:30 +0800

    fixed a few pwoblems (problems)

--- main.ts
+++ main.ts
@@ -1,16 +1,16 @@
 //% color=#37E0FF weight=0 icon="\uf1d8" block="Broadcasts"
 namespace Broadcasts {
     let broadcast_active = -1
-    //% shim=ENUM_GET2983
+    //% shim=ENUM_GET2938
     //% blockId=broadcasts82932938283929323232323
-    //% block="$broad"
+    //% block="$Broadcast"
     //% enumName="Broadcasts"
     //% enumMemberName="Broadcast"
     //% enumPromptHint="Broadcast..."
     //% enumInitialMembers="message1"
     //% blockHidden=true
-    export function dummy (broad: number) {
-        return broad
+    export function dummy (Broadcast: number) {
+        return Broadcast
     }
     //% blockId=stuff032382932
     //% block="broadcast $broadcast"
