69bb3c9b2ae2c9c37f9e904e3b98eba348c46031
Joshua Kordek <79750252+joshkordek3@users.noreply.github.com>
2021-04-07 09:30:44 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,31 @@
+namespace Broadcasts {
+    let broadcast_active = -1
+    //% shim=ENUM_GET2983
+    //% blockId=broadcasts82932938283929323232323
+    //% block="$variable"
+    //% enumName="Broadcasts"
+    //% enumMemberName="Broadcast"
+    //% enumPromptHint="Broadcast..."
+    //% enumInitialMembers="message1"
+    //% blockHidden=true
+    export function dummy (broad: number) {
+        return broad
+    }
+    //% blockId=stuff032382932
+    //% block="broadcast $broadcast"
+    //% broadcast.shadow="broadcasts82932938283929323232323"
+    export function broadcast (broadcast: number) {
+        broadcast_active = broadcast
+    }
+    //% blockId=stuff032382932898
+    //% block="when I receive $broadcast"
+    //% broadcast.shadow="broadcasts82932938283929323232323"
+    export function broadcast_receive (broadcast: number, a: () => void) {
+        basic.forever(function () {
+            if (broadcast_active == broadcast) {
+                broadcast_active = -1
+                control.inBackground(a)
+            }
+        })
+    }
+}
